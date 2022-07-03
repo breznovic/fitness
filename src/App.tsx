a3b451acda84311c2dca2fb22aa8cf5efd22975a
breznovic <slymarkel@gmail.com>
2022-07-03 12:18:07 +0300

    New project

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-function AlternativeAffairs() {
+function App() {
     return (
         <div>
-
+           Website
         </div>
     )
 }
 
-export default AlternativeAffairs
+export default App
